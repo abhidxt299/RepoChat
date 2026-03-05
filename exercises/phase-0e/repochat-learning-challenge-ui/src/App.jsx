835a233ac66e9d5e798cdde1f7459104fe4b24bb
abhidxt299 <abhidxt299@gmail.com>
2026-03-05 19:15:38 +0530

    Learning Challenge - Phase E.1

---
+++ exercises/phase-0e/repochat-learning-challenge-ui/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Your Name</h1>
+      <h3>SDET to MLE Engineer</h3>
+      <p>Building RepoChat to learn RAG and vector databases.</p>
+    </div>
+  )
+}
