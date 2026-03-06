6a8be286877f7977dc316e26ea3b112e4261f14d
abhidxt299 <abhidxt299@gmail.com>
2026-03-06 17:43:52 +0530

    Learning Challenge - Phase E.2

--- exercises/phase-0e/repochat-learning-challenge-ui/src/App.jsx
+++ exercises/phase-0e/repochat-learning-challenge-ui/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { RepoCard } from './utils/RepoCard.jsx'
 
 export default function App() {
   return (
-    <div>
-      <h1>Your Name</h1>
-      <h3>SDET to MLE Engineer</h3>
-      <p>Building RepoChat to learn RAG and vector databases.</p>
-    </div>
+    <>
+      <div className = "profile">
+        <h1>Abhishek Dixit</h1>
+        <h3>SDE to ML Engineer</h3>
+        <p>Building RepoChat to learn RAG and vector databases.</p>
+      </div>
+      <div className = "repo-cards">
+        <RepoCard name="fastapi" stars={75000} />
+        <RepoCard name="react"   stars={220000} />
+        <RepoCard name="flask"   stars={66000} />
+      </div>
+    </>
   )
 }
