5b75af1da98b509296b749d19f7d61f1ce949ccc
abhidxt299 <abhidxt299@gmail.com>
2026-03-10 19:11:27 +0530

    Learning Challenge - Phase E.3

--- exercises/phase-0e/repochat-learning-challenge-ui/src/App.jsx
+++ exercises/phase-0e/repochat-learning-challenge-ui/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import { RepoCard } from './utils/RepoCard.jsx'
+import { useState } from 'react'
 
 export default function App() {
+  
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <div className = "profile">
@@ -13,6 +17,12 @@ export default function App() {
         <RepoCard name="fastapi" stars={75000} />
         <RepoCard name="react"   stars={220000} />
         <RepoCard name="flask"   stars={66000} />
+      </div>
+      <div>
+        <h2> Count: {count} </h2>
+        <button onClick = { () => setCount(count + 1) }> Clicking increases count by 1 </button>
+        <button onClick = { () => setCount(count - 1) }> Clicking decreases count by 1 </button>
+        <button onClick = { () => setCount(0) }> Clicking resets the count </button>
       </div>
     </>
   )
