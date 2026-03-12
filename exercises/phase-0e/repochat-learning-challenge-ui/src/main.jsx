dd6527bc0e61dc7048213351cf41b64d11798364
abhidxt299 <abhidxt299@gmail.com>
2026-03-12 20:06:30 +0530

    Learning Challenge - Phase E.7

--- exercises/phase-0e/repochat-learning-challenge-ui/src/main.jsx
+++ exercises/phase-0e/repochat-learning-challenge-ui/src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import CoinFlip from './CoinFlip.jsx'
 import ListAndKeys from './ListAndKeys.jsx'
+import FetchRepo from './FetchRepo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <CoinFlip />
     <ListAndKeys />
+    <FetchRepo />
   </StrictMode>
 )
